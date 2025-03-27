f4bab45923b6698e9d4596cd728defb8ae00b869
ShoriArai <24wm2101@student.gs.chiba-u.jp>
2025-03-28 00:55:16 +0900

    Add files via upload

--- fly_data.c
+++ fly_data.c
@@ -1,5 +1,10 @@
-//飛行シミュレーションデータを作成するプログラム
-//参考文献：https://journals.le.ac.uk/index.php/pst/article/view/2127
+/*
+ウイングスーツを着た人が空気抵抗、揚力、重力を受けながら運動するときの
+飛行データをテキストファイルで出力するプログラム
+（参考文献：https://journals.le.ac.uk/index.php/pst/article/view/2127）
+出力されるデータは左から、時刻、x座標、y座標、x方向の速度、y方向の速度
+データを用いたグラフの出力にはfly_graph.pyを使う
+*/
 #include <stdio.h>
 
 int main(void){
@@ -7,30 +12,33 @@ int main(void){
     float x, y, Vx, Vy, t, dt, tmax;
     float k0[4], k1[4], k2[4], k3[4];
     float x1, y1, Vx1, Vy1, x2, y2, Vx2, Vy2, x3, y3, Vx3, Vy3;
-    FILE* fp;
+    FILE* fp_in;
+    FILE* fp_out;
 
     R = 1.1;        //翼の下面と上面を横切る速度の比
     m = 70.0;       //飛ぶ人の質量
-    a = 0.001;      //空気抵抗の比例係数
-    b = 0.003;      //空気抵抗の比例係数
+    a = 0.001;      //空気抵抗の比例係数（x方向）
+    b = 0.003;      //空気抵抗の比例係数（y方向）
     x = 0.0;        //初期のx座標
     y = 10.0;       //初期のy座標
     Vy = 0.0;       //y方向の初速度
     tmax = 60.0;    //時間の最大値（無限ループを防ぐ）
     dt = 0.01;      //時間のきざみ幅
-    char fname[] = "fly_data.txt";    //ファイル名
+    char fname_out[] = "fly_data.txt";  //出力ファイル名
     
-
     //Vx：x方向の初速度, S：ウイングスーツの面積, ro：大気の密度, g：重力加速度
-    //例1：Vx = 11.85, S = 1.4, ro = 1.225, g = 9.8
-    //例2：Vx = 10.00, S = 2.0, ro = 5.5, g = 1.4
-    //例3：Vx = 65.00, S = 1.4, ro = 1.225, g = 9.8
-    scanf("%f %f %f %f", &Vx, &S, &ro, &g);
+    //sample1.txt：ウサインボルトは地球で飛べるのか？
+    //sample2.txt：ウサインボルトは土星の衛星タイタンで飛べるのか？
+    //sample3.txt：新幹線ほどの速さで助走をつければ地球でも飛べるのか？
+    //sample4.txt：ウイングスーツの面積を100倍にすれば、地球でも飛べるのか？
+    char fname_in[] = "sample2.txt";  //入力ファイル名（必要に応じて変更）
+    fp_in = fopen(fname_in, "r");
+    fscanf(fp_in,"%f%f%f%f", &Vx, &S, &ro, &g);
 
     k = S * ro * (R * R - 1.0) / 2.0 / m;
 
-    fp = fopen(fname, "w");
-    fprintf(fp, "%8.4f %8.4f %8.4f %8.4f %8.4f\n", 0, x, y, Vx, Vy);
+    fp_out = fopen(fname_out, "w");
+    fprintf(fp_out, "%8.4f %8.4f %8.4f %8.4f %8.4f\n", 0, x, y, Vx, Vy);
     //ルンゲクッタ法を用いた数値計算
     for(t = 0;t < tmax;t += dt){
         if (Vy > 0)   c = -1 * b;
@@ -72,11 +80,11 @@ int main(void){
         Vx = Vx + (k0[2] + 2.0 * k1[2] + 2.0 * k2[2] + k3[2]) / 6.0;
         Vy = Vy + (k0[3] + 2.0 * k1[3] + 2.0 * k2[3] + k3[3]) / 6.0;
         //printf("%8.4f %8.4f %8.4f %8.4f %8.4f\n",t+dt,x,y,Vx,Vy);
-        fprintf(fp, "%8.4f %8.4f %8.4f %8.4f %8.4f\n", t + dt, x, y, Vx, Vy);
+        fprintf(fp_out, "%8.4f %8.4f %8.4f %8.4f %8.4f\n", t + dt, x, y, Vx, Vy);
         if (y < 0) break;
     }
 
-    fclose(fp);
+    fclose(fp_out);
 
     return 0;
 }
--- fly_graph.py
+++ fly_graph.py
@@ -1,23 +1,24 @@
-#作成したデータをもとにグラフを作るプログラム
+#fly_data.cで作ったテキストファイルのデータをもとに人の飛行軌道を画像として保存するプログラム
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams["mathtext.fontset"] = "cm"
 
-fname = "fly_data.txt"  #読み込むファイル名
-gname = "graph.png"     #画像ファイル名
-gformat = "png"         #ファイル形式
-dpi = 300               #画質
+fname = "fly_data.txt"  #入力ファイル名
+gname = "graph.png"     #画像の名前
+gformat = "png"         #画像の保存形式
+dpi = 300               #画像の解像度
 
-data = np.loadtxt(fname)
+data = np.loadtxt(fname)    #データの入力
 t = data[:,0]   #時間
 x = data[:,1]   #x座標
 y = data[:,2]   #y座標
 vx = data[:,3]  #x方向の速度
 vy = data[:,4]  #y方向の速度
 
-plt.plot(x,y)
-plt.title("$\mathrm{Orbit}$", fontsize=20)
+plt.plot(x,y)   #軌道を描画
+#飛行時間をグラフの上に表示
+plt.title("$\mathrm{}\ \mathrm{} = {}$" .format("{flight}","{time}",t[-1]),fontsize=16)
 plt.xlabel("$x$", fontsize=18)
 plt.ylabel("$y$", fontsize=18)
-plt.savefig(gname, format=gformat, dpi=dpi)
-#plt.show()
+plt.savefig(gname, format=gformat, dpi=dpi) #画像の保存
+plt.show()
